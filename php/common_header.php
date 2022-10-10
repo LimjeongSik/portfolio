1e0fd8cb6163f59d8b72eca81582e6e8998b4f71
Jeogn sik <ol37@naver.com>
2022-10-10 22:18:38 +0900

    2022.10.10

--- php/common_header.php
+++ php/common_header.php
@@ -3,6 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="author" content="JeongSik">
   <link rel="shortcut icon" href="#">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/slide.min.css">
